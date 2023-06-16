fe0b1939de3a670c22c455ad493a2dbb74519c2a
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-06-16 15:56:59 +0200

    Add Milestone 3

--- src/org/lessons/java/Mysql.java
+++ src/org/lessons/java/Mysql.java
@@ -1,6 +1,7 @@
 package org.lessons.java;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Mysql {
     public static void main(String[] args) {
@@ -9,29 +10,31 @@ public class Mysql {
         String user = "root";
         String password = "root";
 
-
-        // Definizione della query SQL
-
-
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             System.out.println(con.getCatalog());
+
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Ricerca: ");
+            String search = scan.nextLine();
 
             String sql = """
                     SELECT c.name AS country_name, c.country_id, r.name AS region_name, con.name AS continent_name
                     FROM countries c
                     JOIN regions r ON c.region_id = r.region_id
                     JOIN continents con ON r.continent_id = con.continent_id
+                    WHERE c.name LIKE ?
                     ORDER BY c.name ASC""";
 
             try (PreparedStatement ps = con.prepareStatement(sql)) {
+                ps.setString(1, "%" + search + "%");
                 try (ResultSet rs = ps.executeQuery()) {
                     while (rs.next()) {
-                        String CountryName = rs.getString("country_name");
-                        int CountryId = rs.getInt("country_id");
-                        String RegionName = rs.getString("region_name");
-                        String ContinentName = rs.getString("continent_name");
+                        String countryName = rs.getString("country_name");
+                        int countryId = rs.getInt("country_id");
+                        String regionName = rs.getString("region_name");
+                        String continentName = rs.getString("continent_name");
 
-                        System.out.println(CountryName + "-" + CountryId + "-" + RegionName + "-" + ContinentName);
+                        System.out.println(countryName + "-" + countryId + "-" + regionName + "-" + continentName);
                         System.out.println();
                     }
                 }
